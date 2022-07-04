fca11a848b9e2b88f8021adad74698c6bd256b4e
Swalbak <usually670@gmail.com>
2022-07-04 10:55:58 +0900

    day2

--- leetCode/Array/35. Search Insert Position.py
+++ leetCode/Array/35. Search Insert Position.py
@@ -7,9 +7,30 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        #using binary search
         start=0
         end=len(nums)-1
-
-        while start<end:
+        mid=0
+        while start<=end:
             mid=(start+end)//2
-            if nums[start]<nums[
+            if nums[mid]<target:
+                start=mid+1
+            elif nums[mid]>target:
+                end=mid-1
+            else:
+                return mid
+            
+        #this conditional is already reflected in 'start' variable
+        '''
+        if nums[mid]<target:
+            mid+=1
+        return mid
+        '''
+        return start
+
+
+nums=[1, 3, 5, 6]
+target=5
+a=Solution()
+print(a.searchInsert(nums, target))
+                
